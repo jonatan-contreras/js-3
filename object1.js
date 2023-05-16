01f19f5c7c4108d34c9a924bb6966f371a3d9d0f
Jonatan Contreras <contreras.jonatanfabian@gmail.com>
2023-05-15 19:06:18 -0500

    commit final

---
+++ object1.js
@@ -0,0 +1,21 @@
+var pedro = {
+  nombre: "Pedro Perez",
+  edad: 30,
+  activo: true,
+  hobbies: ["programar", "squash", "piano"],
+};
+
+console.log(pedro.edad);
+
+pedro.estatura = 1.8;
+delete pedro.activo;
+
+for (var key in pedro) {
+  console.log(key + ": " + pedro[key]);
+}
+
+pedro.saluda = function () {
+  return "Hola, me llamo " + this.nombre;
+};
+
+console.log(pedro.saluda());
